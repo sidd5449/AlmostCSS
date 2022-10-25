f51eb47c01a17a1c7e64ede482df8e3417df66da
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-10-25 16:32:10 +0530

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 const {src, dest, watch, series} = require('gulp');
+const GulpCleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass'));
 
+
+var scssFilesPath = './main/assets/css/components/**/*.scss';
+var cssFolder = './css';
 function buildStyles(){
-    return src('index.scss')
-        .pipe(sass())
-        .pipe(dest('css'))
+    return src(scssFilesPath)
+    // .pipe(sass({sassModules: true}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(rename('style.min.css'))
+    .pipe(GulpCleanCss())
+    .pipe(dest(cssFolder))
+    
+    
+        
+
 }
 
 function watchTask(){
-    watch(['index.scss'], buildStyles);
+    watch(scssFilesPath, buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
