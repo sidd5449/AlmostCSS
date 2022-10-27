d913d4db908082130b3b8c68cb6e30e27ec8013b
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-10-27 22:40:00 +0530

    added styles for btns

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // loads various gulp modules
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass-embedded'));
 const { src, dest, watch, series } = require('gulp')
 const concat = require('gulp-concat');
 const minifyCSS = require('gulp-clean-css');
@@ -9,6 +10,7 @@ const rename = require('gulp-rename');
 // create task
 function buildStyles(){
     return src('main/css/**/*.scss')
+      // .pipe(sass().on('error', sass.logError))
         .pipe(minifyCSS())
         // .pipe(rename('style.min.css'))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
